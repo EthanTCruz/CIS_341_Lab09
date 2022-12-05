a71e81fc36e6429b75520f5f6cf7a6a951d49d21
Ethan Cruz <ethancruz3141@gmail.com>
2022-12-05 15:12:41 -0600

    began switching to customer controlloer

--- Lab8/Controllers/CustomerController.cs
+++ Lab8/Controllers/CustomerController.cs
@@ -19,10 +19,7 @@ namespace Lab8.Controllers
             _context = context;
             _userManager = userManager;
         }
-        public IActionResult Index()
-        {
-            return View();
-        }
+
 
         public IActionResult SubmitListing()
         {
@@ -31,10 +28,97 @@ namespace Lab8.Controllers
 
 
 
+        [Authorize]
+        public async Task<IActionResult> Index()
+        {
+            List<Listing> listings = await _context.Listings
+                .Include(listing => listing.Condition)
+                .Include(listing => listing.Store)
+                .ToListAsync();
+
+            foreach (Listing listing in listings)
+            {
+
+                await _context.Entry(listing).Reference(l => l.CreatedBy).LoadAsync();
+            }
+
+            List<ListingDTO> listDTOs = new();
+            foreach (Listing l in listings)
+            {
+                string customerName = "Unclaimed";
+                if (l.ClaimedByID != null)
+                {
+                    customerName = l.ClaimedBy.Name;
+                }
+                ListingDTO listingDTO = new()
+                {
+                    ListingID = l.ListingID,
+                    Quantity = l.Quantity,
+                    Description = l.Description,
+                    CreatedBy = l.CreatedBy.Name,
+                    ClaimedBy = l.ClaimedBy.Name,
+                    Store = l.Store.Name,
+                    Condition = l.Condition.Description
+                };
+                listDTOs.Add(listingDTO);
+            }
+
+            return View(listDTOs);
+        }
+
 
 
         [Authorize]
-        public async Task<IActionResult> ViewListings()
+        [HttpGet]
+        public async Task<IActionResult> Claim_Unclaim(int id, [Bind("ListingID")] ListingDTO listing)
+        {
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    Task<ApplicationUser> GetCurrentUserAsync() => _userManager.GetUserAsync(HttpContext.User);
+
+                    var user = await GetCurrentUserAsync();
+
+                    var actual_customer = await _context.Customers
+.FirstOrDefaultAsync(l => l.Email == user.Email);
+                    var entity = await _context.Listings
+                        .FirstOrDefaultAsync(l => l.ListingID == id);
+                    if (entity.ClaimedByID is null)
+                    {
+
+                        entity.ClaimedBy = actual_customer;
+                    }
+                    else
+                    {
+                        if (entity.ClaimedBy == actual_customer)
+                        {
+                            entity.ClaimedBy = null;
+                        }
+                    }
+                    await _context.SaveChangesAsync();
+
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!_context.Listings.Any(e => e.ListingID == id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return Redirect("/Home/Index");
+            }
+            return Redirect("/Home/Index");
+        }
+
+
+        [Authorize]
+        public async Task<IActionResult> ViewCreatedListings()
         {
             Task<ApplicationUser> GetCurrentUserAsync() => _userManager.GetUserAsync(HttpContext.User);
             
--- Lab8/Program.cs
+++ Lab8/Program.cs
@@ -68,6 +68,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Customer}/{action=Index}/{id?}");
 app.MapRazorPages();
 app.Run();
